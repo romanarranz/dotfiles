a5f23827c243f3c46a5171b7209284608eba9124
romanarranz <burial92@gmail.com>
2022-09-15 11:18:18 +0200

    chore: added protobuf and format

--- starters/start_linux.zsh
+++ starters/start_linux.zsh
@@ -33,6 +33,7 @@ lsb-release \
 ncdu \
 nmap \
 obs-studio \
+protobuf-compiler \
 p7zip-full \
 p7zip-rar \
 scrub \
@@ -51,16 +52,16 @@ mkdir -p $HOME/.local/share/fonts
 
 # Download fonts
 FONT_URLS=(
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+  "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+  "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+  "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+  "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
 )
 
 for font in "${FONT_URLS[@]}"; do
-    filename=$(basename "$font")
-    rm ~/.local/share/fonts/$filename ~/.local/share/fonts/$filename.*
-    wget $font -P ~/.local/share/fonts
+  filename=$(basename "$font")
+  rm ~/.local/share/fonts/$filename ~/.local/share/fonts/$filename.*
+  wget $font -P ~/.local/share/fonts
 done
 
 sudo apt install fonts-firacode
@@ -69,10 +70,10 @@ sudo apt install fonts-firacode
 fc-cache -vf ~/.local/share/fonts/
 
 if [[ $SHELL != "/usr/bin/zsh" ]]; then
-    sudo apt install -y zsh
-    chsh -s $(which zsh)
-    # desktop session logout -- required to reload zsh
-    gnome-session-quit
+  sudo apt install -y zsh
+  chsh -s $(which zsh)
+  # desktop session logout -- required to reload zsh
+  gnome-session-quit
 fi
 
 # Brew System Package manager
@@ -80,18 +81,18 @@ fi
 # Install brew
 BREW=$(which brew)
 if [[ "$BREW" =~ "not found" ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 # ohmyzsh
 if [ ! -d ~/.oh-my-zsh ]; then
-    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 ZOXIDE=$(which zoxide)
 if [[ "$ZOXIDE" =~ "not found" ]]; then
-    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 fi
 
 source $HOME/.zprofile
@@ -128,24 +129,24 @@ GIT_DELTA=$(echo $BREW_PKGS|grep git-delta)
 if [[ "x$GIT_DELTA" = "x" ]]; then LIBS+=("git-delta") fi
 
 if [ ${#TAPS[@]} -gt 0 ]; then
-    brew tap $TAPS
+  brew tap $TAPS
 fi
 
 if [ ${#LIBS[@]} -gt 0 ]; then
-    brew install $LIBS
-    brew cleanup
+  brew install $LIBS
+  brew cleanup
 fi
 
 if [ ${#CASKS[@]} -gt 0 ]; then
-    brew install --cask $CASKS
-    brew cleanup
+  brew install --cask $CASKS
+  brew cleanup
 fi
 
 # Snap packages
 #
 PICK=$(which pick-colour-picker)
 if [[ "$PICK" =~ "not found" ]]; then
-    sudo snap install pick-colour-picker
+  sudo snap install pick-colour-picker
 fi
 
 # Node
@@ -153,36 +154,36 @@ fi
 # nvm
 NVM_DIR=$HOME/.nvm
 if [ ! -d "$NVM_DIR" ]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh|bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh|bash
 fi
 
 NODE_VERSIONS=$(nvm ls --no-colors --no-alias)
 NODE14=$(echo $NODE_VERSIONS|grep v14)
 if [[ "x$NODE14" =~ "not found" ]]; then
-    nvm install 14
+  nvm install 14
 fi
 NODE16=$(echo $NODE_VERSIONS|grep v16)
 if [[ "x$NODE16" =~ "not found" ]]; then
-    nvm install 16
+  nvm install 16
 fi
 NODE=$(which npm)
 if [[ "$NODE" =~ "not found" ]]; then
-    nvm alias default 16
+  nvm alias default 16
 fi
 PNPM=$(which pnpm)
 if [[ "$PNPM" =~ "not found" ]]; then
-    npm i -g pnpm
+  npm i -g pnpm
 fi
 
 TLDR=$(which tldr)
 if [[ "$TLDR" =~ "not found" ]]; then
-    # https://github.com/tldr-pages/tldr
-    npm i -g tldr
+  # https://github.com/tldr-pages/tldr
+  npm i -g tldr
 fi
 CDK_DIA=$(which cdk-dia)
 if [[ "$CDK_DIA" =~ "not found" ]]; then
-    # https://github.com/pistazie/cdk-dia
-    npm i -g cdk-dia
+  # https://github.com/pistazie/cdk-dia
+  npm i -g cdk-dia
 fi
 
 # Ruby
@@ -190,23 +191,23 @@ fi
 # rbenv
 RBENV_DIR=$HOME/.rbenv
 if [ ! -d "$RBENV_DIR" ]; then
-    git clone https://github.com/rbenv/rbenv.git $RBENV_DIR
-    pushd ~/.rbenv && src/configure && make -C src && popd
-    
-    # ruby-build install
-    mkdir -p $RBENV_DIR/plugins
-    git clone https://github.com/rbenv/ruby-build.git $RBENV_DIR/plugins/ruby-build
-    if [ "x$RBENV_ROOT" = "x" ]; then
-        echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ~/.zshrc
-        echo 'export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"' >> ~/.zshrc
-    fi
+  git clone https://github.com/rbenv/rbenv.git $RBENV_DIR
+  pushd ~/.rbenv && src/configure && make -C src && popd
+  
+  # ruby-build install
+  mkdir -p $RBENV_DIR/plugins
+  git clone https://github.com/rbenv/ruby-build.git $RBENV_DIR/plugins/ruby-build
+  if [ "x$RBENV_ROOT" = "x" ]; then
+    echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ~/.zshrc
+    echo 'export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"' >> ~/.zshrc
+  fi
 fi
 source $HOME/.zshrc
 
 # ruby versions
 RUBY312=$(rbenv versions|grep 3.1.2)
 if [ "x$RUBY312" = "x" ]; then
-    rbenv install 3.1.2
+  rbenv install 3.1.2
 fi
 
 # Python
@@ -214,47 +215,47 @@ fi
 # pyenv
 PYENV_DIR=$HOME/.pyenv
 if [ ! -d "$PYENV_DIR" ]; then
-    git clone https://github.com/pyenv/pyenv.git $PYENV_DIR
-    if [ "x$PYENV_ROOT" = "x" ]; then
-        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-        echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-    fi
+  git clone https://github.com/pyenv/pyenv.git $PYENV_DIR
+  if [ "x$PYENV_ROOT" = "x" ]; then
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+  fi
 fi
 source $HOME/.zshrc
 
 PYENV_PLUGINS_DIR=$HOME/.pyenv/plugins
 if [ ! -d "$PYENV_PLUGINS_DIR" ]; then
-    git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
+  git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 fi
 
 # python versions
 PYTHON27=$(pyenv versions|grep 2.7)
 if [ "x$PYTHON27" = "x" ]; then
-    pyenv install 2.7.18
+  pyenv install 2.7.18
 fi
 PYTHON39=$(pyenv versions|grep 3.9)
 if [ "x$PYTHON39" = "x" ]; then
-    pyenv install 3.9.4
+  pyenv install 3.9.4
 fi
 PYTHON=$(python --version)
 if [[ "$PYTHON" =~ "not found" ]]; then
-    pyenv global 3.9.4
+  pyenv global 3.9.4
 fi
 
 # Poetry
 #
 POETRY=$(which poetry)
 if [[ "$POETRY" =~ "not found" ]]; then
-    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-    source $HOME/.zshrc
-    poetry config virtualenvs.in-project true
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+  source $HOME/.zshrc
+  poetry config virtualenvs.in-project true
 fi
 
 # Rust
 #
 RUSTUP=$(which rustup)
 if [[ "$RUSTUP" =~ "not found" ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs|sh -s -- -y
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs|sh -s -- -y
 fi
 
 # Java
@@ -262,131 +263,131 @@ fi
 # sdkman
 SDKMAN_DIR=$HOME/.sdkman
 if [ ! -d $SDKMAN_DIR ]; then
-    curl -s "https://get.sdkman.io" | bash
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
 fi
 
 JAVA_VERSIONS=$(sdk ls java|grep 'installed\|local')
 JAVA17=$(echo $JAVA_VERSIONS|grep 17)
 if [[ "x$JAVA17" = "x" ]]; then
-    sdk install java 17.0.2-tem
+  sdk install java 17.0.2-tem
 fi
 
 # Kubernetes
 #
 ARKADE=$(which arkade)
 if [[ "$ARKADE" =~ "not found" ]]; then
-    pushd /tmp
-    curl -sLS https://dl.get-arkade.dev|sh
-    sudo cp arkade /usr/local/bin/arkade
-    sudo ln -sf /usr/local/bin/arkade /usr/local/bin/ark
-    popd
+  pushd /tmp
+  curl -sLS https://dl.get-arkade.dev|sh
+  sudo cp arkade /usr/local/bin/arkade
+  sudo ln -sf /usr/local/bin/arkade /usr/local/bin/ark
+  popd
 fi
 
 KUBECTL=$(which kubectl)
 if [[ "$KUBECTL" =~ "not found" ]]; then
-    arkade get kubectl
+  arkade get kubectl
 fi
 
 FAAS=$(which faas-cli)
 if [[ "$FAAS" =~ "not found" ]]; then
-    arkade get faas-cli
+  arkade get faas-cli
 fi
 
 KUBETAIL=$(which kubetail)
 if [[ "$KUBETAIL" =~ "not found" ]]; then
-    sudo wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -O /usr/local/bin/kubetail
-    sudo chmod 755 /usr/local/bin/kubetail
+  sudo wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -O /usr/local/bin/kubetail
+  sudo chmod 755 /usr/local/bin/kubetail
 fi
 
 # helm package manager for k8s
 HELM=$(which helm)
 if [[ "$HELM" =~ "not found" ]]; then
-    arkade get helm
+  arkade get helm
 fi
 
 # Cloud
 #
 AWS=$(which aws)
 if [[ "$AWS" =~ "not found" ]]; then
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
 fi
 
 # Profilers
 #
 CGMEMTIME=$(which cgmemtime)
 if [[ "$CGMEMTIME" =~ "not found" ]]; then
-    pushd ~/.local
-    git clone https://github.com/gsauthof/cgmemtime
-    cd cgmemtime
-    make
-    sudo ./cgmemtime --setup -g $(groups $(whoami) | cut -d' ' -f1) --perm 775
-    ln -s $PWD/cgmemtime ~/bin/cgmemtime
-    popd
+  pushd ~/.local
+  git clone https://github.com/gsauthof/cgmemtime
+  cd cgmemtime
+  make
+  sudo ./cgmemtime --setup -g $(groups $(whoami) | cut -d' ' -f1) --perm 775
+  ln -s $PWD/cgmemtime ~/bin/cgmemtime
+  popd
 fi
 
 # Scrapers
 #
 WHATWEB=$(which whatweb)
 if [[ "$WHATWEB" =~ "not found" ]]; then
-    pushd ~/.local
-    wget https://github.com/urbanadventurer/WhatWeb/archive/refs/tags/v0.5.5.tar.gz -O whatweb.tar.gz
-    tar -zxvf whatweb.tar.gz
-    cd WhatWeb-0.5.5/
-    gem install bundler
-    bundle update
-    bundle install
-    ./whatweb --version
-    ln -s $PWD/whatweb ~/bin/whatweb
-    popd
+  pushd ~/.local
+  wget https://github.com/urbanadventurer/WhatWeb/archive/refs/tags/v0.5.5.tar.gz -O whatweb.tar.gz
+  tar -zxvf whatweb.tar.gz
+  cd WhatWeb-0.5.5/
+  gem install bundler
+  bundle update
+  bundle install
+  ./whatweb --version
+  ln -s $PWD/whatweb ~/bin/whatweb
+  popd
 fi
 
 # Docker
 DOCKER=$(which docker)
 if [[ "$DOCKER" =~ "not found" ]]; then
-    sudo apt remove docker-desktop
-    rm -r $HOME/.docker/desktop
-    sudo rm /usr/local/bin/com.docker.cli
-    sudo apt purge docker-desktop
-    rm ~/.config/systemd/user/docker-desktop.service ~/.local/share/systemd/user/docker-desktop.service
-    # add docker's official GPG key
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    # setup apt repository
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    # download and install
-    wget "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.11.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
-    sudo apt-get install ./docker-desktop-4.11.0-amd64.deb
-    # launch
-    systemctl --user start docker-desktop
+  sudo apt remove docker-desktop
+  rm -r $HOME/.docker/desktop
+  sudo rm /usr/local/bin/com.docker.cli
+  sudo apt purge docker-desktop
+  rm ~/.config/systemd/user/docker-desktop.service ~/.local/share/systemd/user/docker-desktop.service
+  # add docker's official GPG key
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  # setup apt repository
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # download and install
+  wget "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.11.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
+  sudo apt-get install ./docker-desktop-4.11.0-amd64.deb
+  # launch
+  systemctl --user start docker-desktop
 fi
 
 LAZYDOCKER=$(which lazydocker)
 if [[ "$LAZYDOCKER" =~ "not found" ]]; then
-    curl "https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh" | bash
+  curl "https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh" | bash
 fi
 
 # Docker images
 #
 CURLIFIREFOX=$(docker images|grep lwthiker/curl-impersonate|grep 0.5-ff)
 if [ "x$CURLIFIREFOX" = "x" ]; then
-    docker pull lwthiker/curl-impersonate:0.5-ff
+  docker pull lwthiker/curl-impersonate:0.5-ff
 fi
 CURLICHROME=$(docker images|grep lwthiker/curl-impersonate|grep 0.5-chrome)
 if [ "x$CURLICHROME" = "x" ]; then
-    docker pull lwthiker/curl-impersonate:0.5-chrome
+  docker pull lwthiker/curl-impersonate:0.5-chrome
 fi
 THEHARVESTER=$(docker images|grep theharvester)
 if [ "x$THEHARVESTER" = "x" ]; then
-    pushd ~/.local
-    if [ ! -d theHarvester ]; then
-        git clone https://github.com/laramies/theHarvester
-    fi
-    cd theHarvester
-    docker build -t theharvester .
-    popd
+  pushd ~/.local
+  if [ ! -d theHarvester ]; then
+    git clone https://github.com/laramies/theHarvester
+  fi
+  cd theHarvester
+  docker build -t theharvester .
+  popd
 fi
--- starters/start_osx.zsh
+++ starters/start_osx.zsh
@@ -3,7 +3,7 @@
 ARCH=$(uname -p)
 APPLE_CHIP="intel"
 if [ "x$SYSTEM" = "xDarwin" ] && [ "$ARCH" = "arm" ]; then
-    APPLE_CHIP="silicon"
+  APPLE_CHIP="silicon"
 fi
 
 mkdir -p $HOME/bin
@@ -15,25 +15,25 @@ mkdir -p $HOME/.local
 # https://laict.medium.com/install-homebrew-on-macos-11-apple-silicon-630f37a74490
 BREW=$(which brew)
 if [ "x$BREW" = "x" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "# Brew env vars" >> $HOME/.zprofile
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "# Brew env vars" >> $HOME/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 fi
 
 if [ "$APPLE_CHIP" = "silicon" ]; then
-    # Install brew intel dedicated to run programs that are not yet natively compiled for ARM
-    IBREW=$(which ibrew)
-    if [ "x$IBREW" = "x" ]; then
-        pushd /usr/local
-        mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-        popd
-    fi
-
-    IBREW_ALIAS=$(grep ibrew $ZSHCONFIG/aliases/.macos)
-    if [ "x$IBREW_ALIAS" = "x" ]; then
-        echo "# Rosetta Intel based brew" >> $ZSHCONFIG/aliases/.macos
-        echo "alias ibrew='arch -x86_64 /usr/local/homebrew/bin/brew'" >> $ZSHCONFIG/aliases/.macos
-    fi
+  # Install brew intel dedicated to run programs that are not yet natively compiled for ARM
+  IBREW=$(which ibrew)
+  if [ "x$IBREW" = "x" ]; then
+    pushd /usr/local
+    mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+    popd
+  fi
+  
+  IBREW_ALIAS=$(grep ibrew $ZSHCONFIG/aliases/.macos)
+  if [ "x$IBREW_ALIAS" = "x" ]; then
+    echo "# Rosetta Intel based brew" >> $ZSHCONFIG/aliases/.macos
+    echo "alias ibrew='arch -x86_64 /usr/local/homebrew/bin/brew'" >> $ZSHCONFIG/aliases/.macos
+  fi
 fi
 
 source $HOME/.zprofile
@@ -44,7 +44,7 @@ source $HOME/.zshrc
 # XCode Command Line tools
 XCODE=$(xcode-select -p)
 if [ "$XCODE" != "/Library/Developer/CommandLineTools" ]; then
-    xcode-select --install
+  xcode-select --install
 fi
 
 # Brew packages
@@ -64,8 +64,8 @@ HELM=$(echo $BREW_PKGS|grep helm)
 if [ "x$HELM" = "x" ]; then LIBS+=("helm") fi
 HEROKU=$(echo $BREW_PKGS|grep heroku)
 if [ "x$HEROKU" = "x" ]; then
-    TAPS+=("heroku/brew")
-    LIBS+=("heroku")
+  TAPS+=("heroku/brew")
+  LIBS+=("heroku")
 fi
 OPENSSL=$(echo $BREW_PKGS|grep openssl)
 if [ "x$OPENSSL" = "x" ]; then LIBS+=("openssl") fi
@@ -149,25 +149,27 @@ AHA=$(echo $BREW_PKGS|grep aha)
 if [ "x$AHA" = "x" ]; then LIBS+=("aha") fi
 JUST=$(echo $BREW_PKGS|grep just)
 if [ "x$JUST" = "x" ]; then LIBS+=("just") fi
+PROTOC=$(echo $BREW_PKGS|grep protobuf)
+if [ "x$PROTOC" = "x" ]; then LIBS+=("protobuf") fi
 
 if [ ${#TAPS[@]} -gt 0 ]; then
-    brew tap $TAPS
+  brew tap $TAPS
 fi
 
 if [ ${#LIBS[@]} -gt 0 ]; then
-    brew install $LIBS
-    brew cleanup
+  brew install $LIBS
+  brew cleanup
 fi
 
 if [ ${#CASKS[@]} -gt 0 ]; then
-    brew install --cask $CASKS
-    brew cleanup
+  brew install --cask $CASKS
+  brew cleanup
 fi
 
 FZF=$HOME/.fzf
 if [ ! -d  "$FZF" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
 fi
 
 # Node
@@ -175,32 +177,32 @@ fi
 # nvm
 NVM_DIR=$HOME/.nvm
 if [ ! -d "$NVM_DIR" ]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh|bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh|bash
 fi
 
 NODE_VERSIONS=$(nvm ls --no-colors --no-alias)
 NODE14=$(echo $NODE_VERSIONS|grep v14)
 if [ "x$NODE14" = "x" ]; then
-    nvm install 14
+  nvm install 14
 fi
 NODE16=$(echo $NODE_VERSIONS|grep v16)
 if [ "x$NODE16" = "x" ]; then
-    nvm install 16
-    nvm use 16
+  nvm install 16
+  nvm use 16
 fi
 TLDR=$(which tldr)
 if [ "x$TLDR" = "x" ]; then
-    # https://github.com/tldr-pages/tldr
-    npm i -g tldr
+  # https://github.com/tldr-pages/tldr
+  npm i -g tldr
 fi
 NCU=$(which ncu)
 if [ "x$NCU" = "x" ]; then
-    npm i -g npm-check-updates
+  npm i -g npm-check-updates
 fi
 CDK_DIA=$(which cdk-dia)
 if [[ "x$CDK_DIA" = "x" ]]; then
-    # https://github.com/pistazie/cdk-dia
-    npm i -g cdk-dia
+  # https://github.com/pistazie/cdk-dia
+  npm i -g cdk-dia
 fi
 
 # Ruby
@@ -208,18 +210,18 @@ fi
 # rbenv
 RBENV_DIR=$HOME/.rbenv
 if [ ! -d "$RBENV_DIR" ]; then
-    git clone https://github.com/rbenv/rbenv.git $RBENV_DIR
-    if [ "x$RBENV_ROOT" = "x" ]; then
-        echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ~/.zshrc
-        echo 'export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"' >> ~/.zshrc
-    fi
+  git clone https://github.com/rbenv/rbenv.git $RBENV_DIR
+  if [ "x$RBENV_ROOT" = "x" ]; then
+    echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ~/.zshrc
+    echo 'export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"' >> ~/.zshrc
+  fi
 fi
 source $HOME/.zshrc
 
 # ruby versions
 RUBY312=$(rbenv versions|grep 3.1.2)
 if [ "x$RUBY312" = "x" ]; then
-    rbenv install 3.1.2
+  rbenv install 3.1.2
 fi
 
 # Python
@@ -227,161 +229,161 @@ fi
 # pyenv
 PYENV_DIR=$HOME/.pyenv
 if [ ! -d "$PYENV_DIR" ]; then
-    git clone https://github.com/pyenv/pyenv.git $PYENV_DIR
-    if [ "x$PYENV_ROOT" = "x" ]; then
-        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-        echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-    fi
+  git clone https://github.com/pyenv/pyenv.git $PYENV_DIR
+  if [ "x$PYENV_ROOT" = "x" ]; then
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+  fi
 fi
 source $HOME/.zshrc
 
 PYENV_PLUGINS_DIR=$HOME/.pyenv/plugins
 if [ ! -d "$PYENV_PLUGINS_DIR" ]; then
-    git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
+  git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 fi
 
 # python versions
 PYTHON27=$(pyenv versions|grep 2.7)
 if [ "x$PYTHON27" = "x" ]; then
-    pyenv install 2.7.18
+  pyenv install 2.7.18
 fi
 PYTHON37=$(pyenv versions|grep 3.7)
 if [ "x$PYTHON37" = "x" ]; then
-    pyenv install 3.7.10
+  pyenv install 3.7.10
 fi
 PYTHON38=$(pyenv versions|grep 3.8)
 if [ "x$PYTHON38" = "x" ] && [ $APPLE_CHIP = "silicon" ]; then
-	# https://github.com/pyenv/pyenv/issues/1768#issuecomment-753756051
-    curl -sSL https://raw.githubusercontent.com/Homebrew/formula-patches/113aa84/python/3.8.3.patch\?full_index\=1|pyenv install --patch 3.8.6
+  # https://github.com/pyenv/pyenv/issues/1768#issuecomment-753756051
+  curl -sSL https://raw.githubusercontent.com/Homebrew/formula-patches/113aa84/python/3.8.3.patch\?full_index\=1|pyenv install --patch 3.8.6
 fi
 PYTHON39=$(pyenv versions|grep 3.9)
 if [ "x$PYTHON39" = "x" ]; then
-    pyenv install 3.9.4
+  pyenv install 3.9.4
 fi
 
 # Poetry
 #
 POETRY=$(which poetry)
 if [ "x$POETRY" = "x" ]; then
-    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-    source $HOME/.zshrc
-    poetry config virtualenvs.in-project true
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+  source $HOME/.zshrc
+  poetry config virtualenvs.in-project true
 fi
 
 # Fonts
 #
 FIRACODE=$(echo $BREW_PKGS|grep font-fira-code)
 if [ "x$FIRACODE" = "x" ]; then
-    brew tap homebrew/cask-fonts
-    brew install --cask font-fira-code
+  brew tap homebrew/cask-fonts
+  brew install --cask font-fira-code
 fi
 
 # Rust
 #
 RUSTUP=$(which rustup)
 if [ "x$RUSTUP" = "x" ]; then
-    # https://doc.bccnsoft.com/docs/rust-1.36.0-docs-html/edition-guide/rust-2018/rustup-for-managing-rust-versions.html
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs|sh
+  # https://doc.bccnsoft.com/docs/rust-1.36.0-docs-html/edition-guide/rust-2018/rustup-for-managing-rust-versions.html
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs|sh
 fi
 
 # Kubernetes
 #
 if [ ! -d /usr/local/bin ]; then
-    sudo mkdir /usr/local/bin
+  sudo mkdir /usr/local/bin
 fi
 
 ARKADE=$(which arkade)
 if [ "x$ARKADE" = "x" ]; then
-    pushd /tmp
-    curl -sLS https://dl.get-arkade.dev|sh
-    sudo cp arkade-darwin /usr/local/bin/arkade
-    sudo ln -sf /usr/local/bin/arkade /usr/local/bin/ark
-    popd
+  pushd /tmp
+  curl -sLS https://dl.get-arkade.dev|sh
+  sudo cp arkade-darwin /usr/local/bin/arkade
+  sudo ln -sf /usr/local/bin/arkade /usr/local/bin/ark
+  popd
 fi
 
 KUBECTL=$(which kubectl)
 if [ "x$KUBECTL" = "x" ]; then
-    arkade get kubectl
+  arkade get kubectl
 fi
 
 FAAS=$(which faas-cli)
 if [ "x$FAAS" = "x" ]; then
-    arkade get faas-cli
+  arkade get faas-cli
 fi
 
 K3SUP=$(which k3sup)
 if [ "x$K3SUP" = "x" ]; then
-    pushd /tmp
-    curl -sLS https://get.k3sup.dev|sh
-    sudo install k3sup /usr/local/bin/
-    popd
+  pushd /tmp
+  curl -sLS https://get.k3sup.dev|sh
+  sudo install k3sup /usr/local/bin/
+  popd
 fi
 
 KUBETAIL=$(which kubetail)
 if [ "x$KUBETAIL" = "x" ]; then
-    sudo wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -O /usr/local/bin/kubetail
-    sudo chmod 755 /usr/local/bin/kubetail
+  sudo wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -O /usr/local/bin/kubetail
+  sudo chmod 755 /usr/local/bin/kubetail
 fi
 
 # helm package manager for k8s
 HELM=$(which helm)
 if [ "x$HELM" = "x" ]; then
-    arkade get helm
+  arkade get helm
 fi
 
 # Cloud
 #
 AWS=$(which aws)
 if [ "x$AWS" = "x" ]; then
-    pip3 install awscli
+  pip3 install awscli
 fi
 
 # Profilers
 #
 CGMEMTIME=$(which cgmemtime)
 if [[ "$CGMEMTIME" =~ "not found" ]]; then
-    pushd ~/.local
-    git clone https://github.com/gsauthof/cgmemtime
-    cd cgmemtime
-    make
-    sudo ./cgmemtime --setup -g $(groups $(whoami) | cut -d' ' -f1) --perm 775
-    ln -s $PWD/cgmemtime ~/bin/cgmemtime
-    popd
+  pushd ~/.local
+  git clone https://github.com/gsauthof/cgmemtime
+  cd cgmemtime
+  make
+  sudo ./cgmemtime --setup -g $(groups $(whoami) | cut -d' ' -f1) --perm 775
+  ln -s $PWD/cgmemtime ~/bin/cgmemtime
+  popd
 fi
 
 # Scrapers
 #
 WHATWEB=$(which whatweb)
 if [[ "$WHATWEB" =~ "not found" ]]; then
-    pushd ~/.local
-    wget https://github.com/urbanadventurer/WhatWeb/archive/refs/tags/v0.5.5.tar.gz -O whatweb.tar.gz
-    tar -zxvf whatweb.tar.gz
-    cd WhatWeb-0.5.5/
-    gem install bundler
-    bundle update
-    bundle install
-    ./whatweb --version
-    ln -s $PWD/whatweb ~/bin/whatweb
-    popd
+  pushd ~/.local
+  wget https://github.com/urbanadventurer/WhatWeb/archive/refs/tags/v0.5.5.tar.gz -O whatweb.tar.gz
+  tar -zxvf whatweb.tar.gz
+  cd WhatWeb-0.5.5/
+  gem install bundler
+  bundle update
+  bundle install
+  ./whatweb --version
+  ln -s $PWD/whatweb ~/bin/whatweb
+  popd
 fi
 
 # Docker images
 #
 CURLIFIREFOX=$(docker images|grep lwthiker/curl-impersonate|grep 0.5-ff)
 if [ "x$CURLIFIREFOX" = "x" ]; then
-    docker pull lwthiker/curl-impersonate:0.5-ff
+  docker pull lwthiker/curl-impersonate:0.5-ff
 fi
 CURLICHROME=$(docker images|grep lwthiker/curl-impersonate|grep 0.5-chrome)
 if [ "x$CURLICHROME" = "x" ]; then
-    docker pull lwthiker/curl-impersonate:0.5-chrome
+  docker pull lwthiker/curl-impersonate:0.5-chrome
 fi
 THEHARVESTER=$(docker images|grep theharvester)
 if [ "x$THEHARVESTER" = "x" ]; then
-    pushd ~/.local
-    if [ ! -d theHarvester ]; then
-        git clone https://github.com/laramies/theHarvester 
-    fi
-    cd theHarvester
-    docker build -t theharvester .
-    popd
+  pushd ~/.local
+  if [ ! -d theHarvester ]; then
+    git clone https://github.com/laramies/theHarvester
+  fi
+  cd theHarvester
+  docker build -t theharvester .
+  popd
 fi
